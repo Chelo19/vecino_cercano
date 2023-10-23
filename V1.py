afd9209d8e0aad31c4a48f475f74381384d6e9d4
Chelo <marcelodeleongo@gmail.com>
2023-10-22 23:22:36 -0600

    Adicion de mejora

--- V1.py
+++ V1.py
@@ -1,6 +1,7 @@
 import math
+import time
 
-file_name = "48nodes.txt"
+file_name = "5nodes.txt"
 nodes = {}
 route = []
 
@@ -44,7 +45,7 @@ def individualProcess(start_node):
             for node_number, coordinates in temp_nodes.items():
                 if(node_number != current_node):
                     current_distance = getDistanceBetween2Points(temp_nodes[current_node], temp_nodes[node_number])
-                    print(f'Disponible: {current_node} hacia {node_number} = {current_distance}')
+                    # print(f'Disponible: {current_node} hacia {node_number} = {current_distance}')     # descomentar esto hace que se muestren las distancias
                     if(current_distance < min_distance):
                         min_distance = current_distance
                         min_distance_node = node_number
@@ -52,35 +53,57 @@ def individualProcess(start_node):
         else:
             current_distance = getDistanceBetween2Points(temp_nodes[current_node], start_node_item)     # esta línea es para regresar al punto inicial
             total_distance += current_distance
-        print(f'Se selecciona: {min_distance_node}')
+        # print(f'Se selecciona: {min_distance_node}')      # descomentar esto hace que se muestre la selección
         route.append(current_node)
         temp_nodes.pop(current_node)
         current_node = min_distance_node
-    print(total_distance)
+    # print(total_distance)     # descomentar esto hace que se muestre la distancia de la selección
     return total_distance
 
-def showResults(start_points_total_distance):
+def showResults(start_points_total_distance, preference):
+    # la mejora que considero conveniente ocurre dentro de esta función, la idea de esta función, seguido de la estructura del resto del código
+    # es que se pueda saber aparte de la mejor ruta, también las siguientes mejores rutas para así poder encontrar, en caso de tener una preferencia,
+    # una ruta que tenga la misma distancia recorrida que la mejor ruta pero que se acerque lo máximo posible a la preferencia del usuario
+
     sorted_data = dict(sorted(start_points_total_distance.items(), key=lambda item: item[1]['result']))
 
     sorted_list = [(key, value) for key, value in sorted_data.items()]
-    for key, value in sorted_list:
-        print(f'Key: {key}, Result: {value["result"]}')
-    
+    # for key, value in sorted_list:        # descomentar esto hace que se muestren los resultados en orden de menor a mayor distancia total recorrida
+    #     print(f'Key: {key}, Result: {value["result"]}')
+
+    first_result = sorted_list[0][1]["result"]
+    equal_results = [(key, value) for key, value in sorted_list if value["result"] == first_result]
+
+    getNodes()
+    min_distance_index = 0
+    min_distance = 999999999
+    for i in range(len(equal_results)):
+        current_distance = getDistanceBetween2Points(nodes[preference], nodes[equal_results[i][0]])
+        if(current_distance < min_distance):
+            min_distance_index = i
+            min_distance = current_distance
+    print('**********************************************')
+    print(f'Mejor resultado a partir de preferencia: {equal_results[min_distance_index]}')
+    print('**********************************************')
+
 def mainProcess():
     getNodes()
     start_points_total_distance = {}
     for i in range(1, len(nodes) + 1):
-        print(f'*** SE INICIA EL PROCESO EMPEZANDO POR {i} ***')
+        # print(f'*** SE INICIA EL PROCESO EMPEZANDO POR {i} ***')     # descomentar esto hace que se muestre el cambio de nodo de inicio
         start_points_total_distance[i] = {
             "result": individualProcess(i),
             "route": list(route)
         }
-        print(route)
+        # print(route)  # descomentar esto hace que se muestre la ruta tomada en ese paso
         route.clear()
-    print(start_points_total_distance)
-    showResults(start_points_total_distance)
+    preference = 4      # aquí es donde se establece el punto de partida de preferencia del usuario
+    showResults(start_points_total_distance, preference)
     
-
-
 if __name__ == "__main__":
+    tic = time.time()
     mainProcess()
+    toc = time.time()
+    tic_toc = toc - tic
+    print(f'Tiempo en encontrar la mejor ruta: {round(tic_toc, 5)}s')
+    print('**********************************************')
