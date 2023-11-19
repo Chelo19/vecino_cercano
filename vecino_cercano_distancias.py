3eac31e74141b2d2c70456a4fa73d3360b19440e
Marcelo Amado De León Gómez <soportevr@ragasa.com.mx>
2023-11-19 14:07:33 -0600

    Listo vecino cercano y vrp

--- vecino_cercano_distancias.py
+++ vecino_cercano_distancias.py
@@ -1,43 +1,61 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_excel('ftv33.xlsx', index_col=0)
+# Se cargan los datos
+nodes_df = pd.read_excel('files/E_n13_k4_coords.xlsx', index_col=0)
+demanda_df = pd.read_excel('files/E_n13_k4_demanda.xlsx', index_col=0)
+with open('files/E_n13_k4_cap.txt', 'r') as file:
+    capacity = int(file.read())
 
-matrix = data.values
+# Matriz de distancias entre nodos
+matrix = nodes_df.values
 
-def nearest_neighbor(matrix, start_node):
+def nearest_neighbor(matrix, start_node, capacity):
     n = len(matrix)
     visited = [False] * n
-    path = [start_node]
-    current_node = start_node
+    routes = []
     total_distance = 0
 
-    while len(path) < n:
-        visited[current_node] = True
-        nearest_node = None
-        min_distance = np.inf
+    while np.sum(visited) < n:  # Mientras no se hayan visitado todos los nodos
+        path = []
+        current_node = start_node
+        current_capacity = demanda_df.iloc[start_node]['Demanda']  # Capacidad del nodo de inicio
 
-        for i in range(n):
-            if i != current_node and not visited[i] and matrix[current_node][i] < min_distance:
-                min_distance = matrix[current_node][i]
-                nearest_node = i
+        while True:
+            visited[current_node] = True
+            path.append(current_node)
+            nearest_node = None
+            min_distance = np.inf
 
-        if nearest_node is not None:
-            total_distance += matrix[current_node][nearest_node]
-            path.append(nearest_node)
-            current_node = nearest_node
-        else:
-            break
+            for i in range(n):
+                if i != current_node and not visited[i] and matrix[current_node][i] < min_distance \
+                        and current_capacity + demanda_df.iloc[i]['Demanda'] <= capacity:
+                    min_distance = matrix[current_node][i]
+                    nearest_node = i
 
-    total_distance += matrix[current_node][start_node]
-    path.append(start_node)
+            if nearest_node is not None:
+                total_distance += matrix[current_node][nearest_node]
+                current_node = nearest_node
+                current_capacity += demanda_df.iloc[nearest_node]['Demanda']
+            else:
+                path.append(start_node)  # Regresar al nodo inicial para completar la ruta
+                total_distance += matrix[current_node][start_node]
+                break
 
-    return path, total_distance
+        routes.append((path, current_capacity))
 
+        for i, visited_node in enumerate(visited):
+            if not visited_node:
+                start_node = i
+                break
 
-start_node = 12
+    return routes, total_distance
 
-path, total_distance = nearest_neighbor(matrix, start_node)
+start_node = 0
 
-print("Ruta:", path)
-print("Distancia:", total_distance)
+routes, total_distance = nearest_neighbor(matrix, start_node, capacity)
+
+print("Rutas:")
+for i, (route, capacity) in enumerate(routes):
+    print(f"Ruta {i+1}: {route}, Capacidad acumulada: {capacity}")
+print("Distancia total:", total_distance)
